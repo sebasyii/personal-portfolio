fc7d7b03ccb48bf82a2e5b4dac44a556a35b73bc
sebasyii <sebastian.yii.xh@gmail.com>
2020-02-24 20:31:13 +0800

    The First Commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
